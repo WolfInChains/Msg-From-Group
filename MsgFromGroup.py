37da1e3367dc300d23882a2a55ae8adbe42b1a3c
Wolf In Chains <47662957+WolfInChains@users.noreply.github.com>
2020-07-12 18:22:58 +0200

    Update MsgFromGroup.py

--- MsgFromGroup.py
+++ MsgFromGroup.py
@@ -1,7 +1,5 @@
 import vk_api
 
-from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
-
 f = open("authorization.txt", "r")
 data = f.read()
 line = data.split(':')
@@ -11,7 +9,6 @@ group_token = line[1]
 vk = vk_api.VkApi(token=group_token)
 vk._auth_token()
 vk.get_api()
-longpoll = VkBotLongPoll(vk, group_id)
 
 def send_msg(chat_id: int, message: str, attachment: str = ""):
     return vk.method("messages.send", {**locals(), "random_id": 0})
